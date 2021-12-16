1c14746e22b72ac16313d890234ccb3c7c671108
Zaplatynskyy <zaplatynskyy@gmail.com>
2021-12-16 16:20:48 +0100

    init box

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,9 +1,16 @@
 <template>
     <main class="debug">
         <div class="container py-5">
-            <div class="row">
-                <div class="col-12">
-                    <div class="box"></div>
+            <div class="row gy-3">
+                <div class="col" v-for="(music, i) in musics" :key="i">
+                    <div class="box">
+                        <img src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="">
+
+                        <h2>New Jersey</h2>
+                        
+                        <div class="name">Bon Jovi</div>
+                        <div class="year">1988</div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -22,7 +29,7 @@ export default {
     created() {
         axios.get('https://flynn.boolean.careers/exercises/api/array/music')
             .then((response) => {
-                this.musics = response.data
+                this.musics = response.data.response
             })
     }
 }
@@ -33,13 +40,19 @@ export default {
     main {
         height: calc(100vh - 70px);
         background-color: #1e2d3b;
+        overflow: auto;
     }
 
     // DEBUG
     .debug .box {
         width: 200px;
         height: 300px;
+        text-align: center;
         background-color: red;
+        padding: 10px;
+
+        img {
+            width: 100%;       }
     }
     
 </style>
