31ed96a6acc37fa1fd4c97174e8f71ec7e3463fe
Zaplatynskyy <zaplatynskyy@gmail.com>
2021-12-16 16:01:15 +0100

    Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+    <header>
+        <img src="../assets/img/logo.png" alt="">
+    </header>
+</template>
+
+<script>
+export default {
+    name : 'Header'
+}
+</script>
+
+<style lang="scss" scoped>
+    header {
+        height: 70px;
+        background-color: #2e3a46;
+
+        img {
+            height: 100%;
+            padding: 10px;
+        }
+    }
+</style>
